de6e756a0e7e6c92470bae4b31285603c83dc395
Larry Gritz <larrygritz@gmail.com>
2009-05-02 00:55:05 +0000

    Minor refactor -- break unweildy inline method defs out of the class declaration.
    git-svn-id: https://openshadinglanguage.googlecode.com/svn/trunk@66 a3683778-fedd-11de-87c3-653bb6514e15

--- src/liboslquery/oslquery.cpp
+++ src/liboslquery/oslquery.cpp
@@ -95,89 +95,13 @@ public:
     { }
     virtual ~OSOReaderQuery () { }
     virtual void version (const char *specid, float version) { }
-    virtual void shader (const char *shadertype, const char *name) {
-        m_query.m_shadername = name;
-        m_query.m_shadertype = shadertype;
-    }
-    virtual void symbol (SymType symtype, TypeSpec typespec, const char *name)
-    {
-        if (symtype == SymTypeParam || symtype == SymTypeOutputParam) {
-            m_reading_param = true;
-            OSLQuery::Parameter p;
-            p.name = name;
-            p.type = typespec.simpletype();   // FIXME -- struct & closure
-            p.isoutput = (symtype == SymTypeOutputParam);
-            p.varlenarray = (typespec.arraylength() < 0);
-            p.isstruct = typespec.is_structure();
-            p.isclosure = typespec.is_closure();
-            m_query.m_params.push_back (p);
-        } else {
-            m_reading_param = false;
-        }
-    }
-    virtual void symdefault (int def) {
-        if (m_reading_param && m_query.nparams() > 0) {
-            OSLQuery::Parameter &p (m_query.m_params[m_query.nparams()-1]);
-            if (p.type.basetype == TypeDesc::FLOAT)
-                p.fdefault.push_back ((float)def);
-            else
-                p.idefault.push_back (def);
-            p.validdefault = true;
-        }
-    }
-    virtual void symdefault (float def) {
-        if (m_reading_param && m_query.nparams() > 0) {
-            OSLQuery::Parameter &p (m_query.m_params[m_query.nparams()-1]);
-            p.fdefault.push_back (def);
-            p.validdefault = true;
-        }
-    }
-    virtual void symdefault (const char *def) {
-        if (m_reading_param && m_query.nparams() > 0) {
-            OSLQuery::Parameter &p (m_query.m_params[m_query.nparams()-1]);
-            p.sdefault.push_back (std::string(def));
-            p.validdefault = true;
-        }
-    }
-    virtual void hint (const char *hintstring) {
-        if (m_reading_param && ! strncmp (hintstring, "%meta{", 6)) {
-            hintstring += 6;
-            // std::cerr << "  Metadata '" << hintstring << "'\n";
-            std::string type = readuntil (&hintstring, ',', '}');
-            std::string name = readuntil (&hintstring, ',', '}');
-            // std::cerr << "    " << name << " : " << type << "\n";
-            OSLQuery::Parameter p;
-            p.name = name;
-            p.type = string_to_type (type.c_str());
-            if (p.type.basetype == TypeDesc::STRING) {
-                while (*hintstring == ' ')
-                    ++hintstring;
-                while (hintstring[0] == '\"') {
-                    ++hintstring;
-                    p.sdefault.push_back (readuntil (&hintstring, '\"'));
-                }
-            } else if (p.type.basetype == TypeDesc::INT) {
-                while (*hintstring == ' ')
-                    ++hintstring;
-                while (*hintstring && *hintstring != '}') {
-                    p.idefault.push_back (atoi (hintstring));
-                    readuntil (&hintstring, ',', '}');
-                }
-            } else if (p.type.basetype == TypeDesc::FLOAT) {
-                while (*hintstring == ' ')
-                    ++hintstring;
-                while (*hintstring && *hintstring != '}') {
-                    p.fdefault.push_back (atof (hintstring));
-                    readuntil (&hintstring, ',', '}');
-                }
-            }
-            m_query.m_params[m_query.nparams()-1].metadata.push_back (p);
-        }
-        // std::cerr << "Hint '" << hintstring << "'\n";
-    }
-    virtual void codemarker (const char *name) {
-        m_reading_param = false;
-    }
+    virtual void shader (const char *shadertype, const char *name);
+    virtual void symbol (SymType symtype, TypeSpec typespec, const char *name);
+    virtual void symdefault (int def);
+    virtual void symdefault (float def);
+    virtual void symdefault (const char *def);
+    virtual void hint (const char *hintstring);
+    virtual void codemarker (const char *name);
     virtual void instruction (int label, const char *opcode) { }
     virtual void instruction_arg (const char *name) { }
     virtual void instruction_jump (int target) { }
@@ -186,6 +110,123 @@ private:
     OSLQuery &m_query;
     bool m_reading_param;     // Are we reading a param now?
 };
+
+
+
+void
+OSOReaderQuery::shader (const char *shadertype, const char *name)
+{
+    m_query.m_shadername = name;
+    m_query.m_shadertype = shadertype;
+}
+
+
+
+void
+OSOReaderQuery::symbol (SymType symtype, TypeSpec typespec, const char *name)
+{
+    if (symtype == SymTypeParam || symtype == SymTypeOutputParam) {
+        m_reading_param = true;
+        OSLQuery::Parameter p;
+        p.name = name;
+        p.type = typespec.simpletype();   // FIXME -- struct & closure
+        p.isoutput = (symtype == SymTypeOutputParam);
+        p.varlenarray = (typespec.arraylength() < 0);
+        p.isstruct = typespec.is_structure();
+        p.isclosure = typespec.is_closure();
+        m_query.m_params.push_back (p);
+    } else {
+        m_reading_param = false;
+    }
+}
+
+
+
+void
+OSOReaderQuery::symdefault (int def)
+{
+    if (m_reading_param && m_query.nparams() > 0) {
+        OSLQuery::Parameter &p (m_query.m_params[m_query.nparams()-1]);
+        if (p.type.basetype == TypeDesc::FLOAT)
+            p.fdefault.push_back ((float)def);
+        else
+            p.idefault.push_back (def);
+        p.validdefault = true;
+    }
+}
+
+
+
+void
+OSOReaderQuery::symdefault (float def)
+{
+    if (m_reading_param && m_query.nparams() > 0) {
+        OSLQuery::Parameter &p (m_query.m_params[m_query.nparams()-1]);
+        p.fdefault.push_back (def);
+        p.validdefault = true;
+    }
+}
+
+
+
+void
+OSOReaderQuery::symdefault (const char *def)
+{
+    if (m_reading_param && m_query.nparams() > 0) {
+        OSLQuery::Parameter &p (m_query.m_params[m_query.nparams()-1]);
+        p.sdefault.push_back (std::string(def));
+        p.validdefault = true;
+    }
+}
+
+
+
+void
+OSOReaderQuery::hint (const char *hintstring)
+{
+    if (m_reading_param && ! strncmp (hintstring, "%meta{", 6)) {
+        hintstring += 6;
+        // std::cerr << "  Metadata '" << hintstring << "'\n";
+        std::string type = readuntil (&hintstring, ',', '}');
+        std::string name = readuntil (&hintstring, ',', '}');
+        // std::cerr << "    " << name << " : " << type << "\n";
+        OSLQuery::Parameter p;
+        p.name = name;
+        p.type = string_to_type (type.c_str());
+        if (p.type.basetype == TypeDesc::STRING) {
+            while (*hintstring == ' ')
+                ++hintstring;
+            while (hintstring[0] == '\"') {
+                ++hintstring;
+                p.sdefault.push_back (readuntil (&hintstring, '\"'));
+            }
+        } else if (p.type.basetype == TypeDesc::INT) {
+            while (*hintstring == ' ')
+                ++hintstring;
+            while (*hintstring && *hintstring != '}') {
+                p.idefault.push_back (atoi (hintstring));
+                readuntil (&hintstring, ',', '}');
+            }
+        } else if (p.type.basetype == TypeDesc::FLOAT) {
+            while (*hintstring == ' ')
+                ++hintstring;
+            while (*hintstring && *hintstring != '}') {
+                p.fdefault.push_back (atof (hintstring));
+                readuntil (&hintstring, ',', '}');
+            }
+        }
+        m_query.m_params[m_query.nparams()-1].metadata.push_back (p);
+    }
+    // std::cerr << "Hint '" << hintstring << "'\n";
+}
+
+
+
+void
+OSOReaderQuery::codemarker (const char *name)
+{
+    m_reading_param = false;
+}
 
 
 };  // end namespace OSL::pvt

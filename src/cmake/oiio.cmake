09ce17ac99f9b14e52fcc5b9c7dbea75fd530631
Larry Gritz <larrygritz@gmail.com>
2009-09-30 18:02:29 +0000

    For SPI, grab OIIO from SpComp2 if the IMAGEIOHOME env variable is not set.
    git-svn-id: https://openshadinglanguage.googlecode.com/svn/trunk@270 a3683778-fedd-11de-87c3-653bb6514e15

--- src/cmake/oiio.cmake
+++ src/cmake/oiio.cmake
@@ -1,15 +1,21 @@
 ###########################################################################
 # OpenImageIO
 
+MESSAGE ( STATUS "OPENIMAGEIOHOME = ${OPENIMAGEIOHOME}" )
+
 find_library ( OPENIMAGEIO_LIBRARY
                NAMES OpenImageIO
-               PATHS $ENV{IMAGEIOHOME}/lib/ )
+               PATHS $ENV{IMAGEIOHOME}/lib/ ${OPENIMAGEIOHOME}/lib )
 find_path ( OPENIMAGEIO_INCLUDES OpenImageIO/imageio.h
-            $ENV{IMAGEIOHOME}/include )
+            $ENV{IMAGEIOHOME}/include ${OPENIMAGEIOHOME}/include )
 IF (OPENIMAGEIO_INCLUDES AND OPENIMAGEIO_LIBRARY )
     SET ( OPENIMAGEIO_FOUND TRUE )
     MESSAGE ( STATUS "OpenImageIO includes = ${OPENIMAGEIO_INCLUDES}" )
     MESSAGE ( STATUS "OpenImageIO library = ${OPENIMAGEIO_LIBRARY}" )
+    MESSAGE ( STATUS "OpenImageIO namespace = ${OPENIMAGEIO_NAMESPACE}" )
+    if (OPENIMAGEIO_NAMESPACE)
+        add_definitions ("-DOPENIMAGEIO_NAMESPACE=${OPENIMAGEIO_NAMESPACE}")
+    endif ()
 ELSE ()
     MESSAGE ( STATUS "OpenImageIO not found" )
 ENDIF ()

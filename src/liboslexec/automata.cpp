5e40420c42eb7c5df2c931f8582129d96751bb47
Chris Kulla <ckulla@gmail.com>
2010-05-19 18:45:44 +0000

    Fix broken build with gcc 4.4+
    git-svn-id: https://openshadinglanguage.googlecode.com/svn/trunk@635 a3683778-fedd-11de-87c3-653bb6514e15

--- src/liboslexec/automata.cpp
+++ src/liboslexec/automata.cpp
@@ -29,6 +29,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "automata.h"
 #include "optautomata.h"
 #include <algorithm>
+#include <cstdio>
 
 
 #ifdef OSL_NAMESPACE

32c0782e6e8993d9f4e9b7e75c97c1cfe90c5942
Larry Gritz <lg@larrygritz.com>
2011-06-17 16:34:43 -0700

    Fix testshade bug where we were computing derivs subtly wrong all along.  Also, fix all the texture tests to shade 'image-like' rather than 'grid-like' and update reference images for anything that changed due to these two items.

--- testsuite/texture-alpha/run.py
+++ testsuite/texture-alpha/run.py
@@ -11,7 +11,7 @@ if len(sys.argv) > 2 :
 
 # A command to run
 command = path + "oslc/oslc test.osl > out.txt"
-command = command + "; " + path + "testshade/testshade -g 128 128 -od uint8 -o Cout out.tif test >> out.txt"
+command = command + "; " + path + "testshade/testshade -g 128 128 --center -od uint8 -o Cout out.tif test >> out.txt"
 
 # Outputs to check against references
 outputs = [ "out.txt", "out.tif" ]

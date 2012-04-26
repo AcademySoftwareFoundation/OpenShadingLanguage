8d0c47f5a5c5637b6f4f39d5dbbd6e9ba9fe210e
Larry Gritz <lg@larrygritz.com>
2012-04-25 22:15:29 -0700

    Fix field3d test to run properly

--- testsuite/texture-field3d/run.py
+++ testsuite/texture-field3d/run.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python 
 
-command += testshade("-g 256 256 -od uint8 -o Cout out.exr test")
+command += testshade("-g 256 256 -od uint8 -o Cout out.tif test")
 outputs = [ "out.txt", "out.tif" ]

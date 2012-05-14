ca48e4ad47c526562e0008a1f513bff70137721c
Larry Gritz <lg@larrygritz.com>
2012-05-14 14:04:05 -0700

    Augment testsuite/texture-wrap to test 'default' wrap from the file

--- testsuite/texture-wrap/run.py
+++ testsuite/texture-wrap/run.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python 
 
-command += testshade("-g 64 64 --center -od uint8 -o Cblack black.tif -o Cclamp clamp.tif -o Cperiodic periodic.tif -o Cmirror mirror.tif test")
+command += "maketx ../common/textures/mandrill.tif --wrap clamp -o mandrill.tx ; \n"
+command += testshade("-g 64 64 --center -od uint8 -o Cblack black.tif -o Cclamp clamp.tif -o Cperiodic periodic.tif -o Cmirror mirror.tif -o Cdefault default.tif test")
 
-outputs = [ "out.txt", "black.tif", "clamp.tif", "periodic.tif", "mirror.tif" ]
+outputs = [ "out.txt", "black.tif", "clamp.tif", "periodic.tif", "mirror.tif",
+            "default.tif" ]

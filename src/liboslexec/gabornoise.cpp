dc38bca9f8578b1b5bae6b83baf177fd039720fe
Larry Gritz <lg@larrygritz.com>
2012-11-02 14:30:48 -0700

    Fix constant that was making errors on 32 bit platforms

--- src/liboslexec/gabornoise.cpp
+++ src/liboslexec/gabornoise.cpp
@@ -69,7 +69,7 @@ public:
     }
     // Return uniform on [0,1)
     float operator() () {
-        return (m_seed *= 3039177861) / float(UINT_MAX);
+        return (m_seed *= 3039177861u) / float(UINT_MAX);
     }
     // Return poisson distribution with the given mean
     int poisson (float mean) {

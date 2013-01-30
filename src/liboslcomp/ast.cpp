415ddc8d1518af5ff954495f6ae7db0f70ccfa54
Larry Gritz <lg@larrygritz.com>
2013-01-30 12:21:30 -0800

    Don't error/warn if metadata name masks a global scope name. Who cares?
    This came up after the recent change in published metadata conventions --
    we now recommend 'min' and 'max', which were generating spurious errors.

--- src/liboslcomp/ast.cpp
+++ src/liboslcomp/ast.cpp
@@ -314,7 +314,7 @@ ASTvariable_declaration::ASTvariable_declaration (OSLCompilerImpl *comp,
 {
     m_typespec = type;
     Symbol *f = comp->symtab().clash (name);
-    if (f) {
+    if (f  &&  ! m_ismetadata) {
         std::string e = Strutil::format ("\"%s\" already declared in this scope", name.c_str());
         if (f->node()) {
             boost::filesystem::path p(f->node()->sourcefile().string());

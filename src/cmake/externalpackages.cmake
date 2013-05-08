85bcea9e61e4a55fb4de969b6525882f52516b89
Larry Gritz <lg@larrygritz.com>
2013-05-08 12:15:33 -0700

    Get rid of TBB references

--- src/cmake/externalpackages.cmake
+++ src/cmake/externalpackages.cmake
@@ -104,53 +104,6 @@ link_directories ("${Boost_LIBRARY_DIRS}")
 
 
 ###########################################################################
-# TBB (Intel Thread Building Blocks) setup
-
-setup_path (TBB_HOME "${THIRD_PARTY_TOOLS_HOME}"
-            "Location of the TBB library install")
-mark_as_advanced (TBB_HOME)
-if (USE_TBB)
-    set (TBB_VERSION 22_004oss)
-    if (MSVC)
-        find_library (TBB_LIBRARY
-                      NAMES tbb
-                      PATHS ${TBB_HOME}/lib
-                      PATHS ${THIRD_PARTY_TOOLS_HOME}/lib/
-                      ${TBB_HOME}/tbb-${TBB_VERSION}/lib/
-                     )
-        find_library (TBB_DEBUG_LIBRARY
-                      NAMES tbb_debug
-                      PATHS ${TBB_HOME}/lib
-                      PATHS ${THIRD_PARTY_TOOLS_HOME}/lib/
-                      ${TBB_HOME}/tbb-${TBB_VERSION}/lib/)
-    endif (MSVC)
-    find_path (TBB_INCLUDES tbb/tbb_stddef.h
-               ${TBB_HOME}/include/tbb${TBB_VERSION}
-               ${THIRD_PARTY_TOOLS}/include/tbb${TBB_VERSION}
-               ${PROJECT_SOURCE_DIR}/include
-               ${OPENIMAGEIOHOME}/include/OpenImageIO
-              )
-    if (TBB_INCLUDES OR TBB_LIBRARY)
-        set (TBB_FOUND TRUE)
-        add_definitions ("-DUSE_TBB=1")
-        if (VERBOSE)
-            message (STATUS "TBB includes = ${TBB_INCLUDES}")
-            message (STATUS "TBB library = ${TBB_LIBRARY}")
-        endif ()
-    else ()
-        message (STATUS "TBB not found")
-    endif ()
-else ()
-    add_definitions ("-DUSE_TBB=0")
-    message (STATUS "TBB will not be used")
-    set(TBB_INCLUDES "")
-    set(TBB_LIBRARY "")
-endif ()
-
-# end TBB setup
-###########################################################################
-
-###########################################################################
 # Partio
 
 find_package (ZLIB)

19d91ff1459fc4670ab0a78e250e0f3f3d49f89d
Larry Gritz <lg@larrygritz.com>
2017-10-18 10:58:55 -0700

    Dual refactor and generalization
    In this patch, I completely overhaul the main class that implements our
    dual arithmetic-based automatic differentiation. No more Dual2, with
    hard-coded x and y partials. Now we have Dual<T,P>, templated on the
    number of partial dimensions P. Now Dual2<T> is just a templated typedef
    that specilizes to 2 dimensions.
    
    In addition to general cleanup, this leads to several benefits:
    
    1. It is now trivial to extend to 3 dimensions (dx, dy, dz) if we wanted
    to use Dual for automatic differentiation of volumetric functions.
    
    2. Dual<T,1> can be used for automatic differentiation of 1-D functions,
    which doesn't have any applications within OSL that I'm aware of, but
    makes it a whole lot more convenient to re-use outside of OSL, any time
    you want to use A.D. with these techniques.
    
    3. It sets us up for a potential future distribution of the Dual stuff
    as a separate library, if it seems useful.  It's not 100% free of other
    dependencies from OSL (& OIIO), but it's fairly close and would not be
    hard to make it entirely self-contained (and header only). I don't need
    to do that this week, but it's something I'd like to work toward.
    
    I did double check this by benchmarking full renders, and have confirmed
    that it doesn't affect performance at all.

---
+++ src/liboslexec/dual_test.cpp
@@ -0,0 +1,80 @@
+/*
+Copyright (c) 2017 Sony Pictures Imageworks Inc., et al.
+All Rights Reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are
+met:
+* Redistributions of source code must retain the above copyright
+  notice, this list of conditions and the following disclaimer.
+* Redistributions in binary form must reproduce the above copyright
+  notice, this list of conditions and the following disclaimer in the
+  documentation and/or other materials provided with the distribution.
+* Neither the name of Sony Pictures Imageworks nor the names of its
+  contributors may be used to endorse or promote products derived from
+  this software without specific prior written permission.
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#include <type_traits>
+
+#include <OpenImageIO/dassert.h>
+#include <OpenImageIO/unittest.h>
+
+#include <OSL/oslconfig.h>
+#include <OSL/dual.h>
+#include <OSL/dual_vec.h>
+
+using namespace OSL;
+
+// Fix namespace problem with OIIO unit tests in older versions.
+#if OIIO_VERSION < 10901
+using namespace OIIO;
+#endif
+
+typedef Dual<float,1> Dualf;
+typedef Dual2<float> Dual2f;
+
+
+
+void
+test_metaprogramming ()
+{
+    // Does is_Dual<> correctly discern duals from non-duals?
+    OIIO_CHECK_ASSERT (is_Dual<Dualf>());
+    OIIO_CHECK_ASSERT (is_Dual<Dual2f>());
+    OIIO_CHECK_ASSERT (! is_Dual<float>());
+    OIIO_CHECK_ASSERT (! is_Dual<Vec3>());
+
+    // Does Dualify<> correctly turn a scalar into a dual?
+    OIIO_CHECK_ASSERT ((std::is_same<Dualify<float>::type, Dualf>::value));
+    OIIO_CHECK_ASSERT ((std::is_same<Dualify<float,2>::type, Dual2f>::value));
+    OIIO_CHECK_ASSERT ((std::is_same<Dualify<Dualf,1>::type, Dualf>::value));
+    OIIO_CHECK_ASSERT ((std::is_same<Dualify<Dual2f,2>::type, Dual2f>::value));
+
+    OIIO_CHECK_ASSERT ((std::is_same<UnDual<float >::type, float>::value));
+    OIIO_CHECK_ASSERT ((std::is_same<UnDual<Vec3  >::type, Vec3> ::value));
+    OIIO_CHECK_ASSERT ((std::is_same<UnDual<Dualf >::type, float>::value));
+    OIIO_CHECK_ASSERT ((std::is_same<UnDual<Dual2f>::type, float>::value));
+}
+
+
+
+int main(int argc, char *argv[])
+{
+    test_metaprogramming ();
+
+    // FIXME: Some day, expand to more exhaustive tests of Dual
+
+    return 0;
+}

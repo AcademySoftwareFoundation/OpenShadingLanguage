2781a7ed48a4aa75f2dd730eacf9f0919cee89b0
Frederich Munch <marsupial@users.noreply.github.com>
2017-12-17 22:08:20 -0500

    Fix poorly defined behavior when choosing certain function overloads. Previously the order of overload declarations would affect which was chosen.

---
+++ testsuite/function-overloads/a_ivp.h
@@ -0,0 +1,11 @@
+
+void testA(int a, float b, float c) {
+    printf("testA int\n");
+}
+void testA(vector a, float b, float c) {
+    printf("testA vector\n");
+}
+void testA(point a, float b, float c) {
+    printf("testA point\n");
+}
+

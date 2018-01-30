3fba07c5388c03fb2b4b6653b9cc0286adb85c8f
Frederich Munch <marsupial@users.noreply.github.com>
2018-01-29 19:33:58 -0500

    Add additional component connections from render-components and remove it. Fix connect-components test not testing C_rgb_separate_in. Remove stray .oso files in connect-components.

--- testsuite/render-components/run.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-
-failthresh = 0.03   # allow a little more LSB noise between platforms
-failpercent = .5
-outputs = [ "out.exr" ]
-command = testrender("-r 320 240 -aa 4 scene.xml out.exr")

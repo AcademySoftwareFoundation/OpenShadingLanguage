bed59ba011cdec1cf14e68cdffcbfee776c31fa9
Tim Grant <tgrant@nvidia.com>
2018-02-27 19:50:47 -0700

    Update the testsuite to reflect the current state of testoptix.

--- testsuite/testoptix/run.py
+++ testsuite/testoptix/run.py
@@ -3,4 +3,4 @@
 failthresh = 0.03   # allow a little more LSB noise between platforms
 failpercent = .5
 outputs = [ "out.exr" ]
-command = testoptix("-r 320 240 out.exr")
+command = testoptix("-r 320 240 scene.xml out.exr")

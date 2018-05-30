1ae5cbd7dc1b972a20c1de00868c6c434a1d1e6b
Tim Grant <tgrant@nvidia.com>
2018-05-30 15:44:08 -0600

    Add detailed comment to device_string.h.

--- src/include/OSL/device_string.h
+++ src/include/OSL/device_string.h
@@ -30,28 +30,58 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #include <OSL/oslconfig.h>
 
+
+// A device_string is a special string representation intended for use on GPUs,
+// which do not readily support ustrings.
+//
+// Two device_strings can be tested for equality simply by comparing their m_tag
+// fields; m_chars is available for cases where the actual string contents are
+// required (e.g., for printf).
+//
+// For comparisons between device_strings to be valid, OSL library code
+// (shadeops, noise gen, etc.), compiled shaders, and renderer application code
+// must all agree on the tag for each string.
+//
+// The other design goal is that compiled shaders containing strings can be
+// cached, because tags are consistent from run to run.
+//
+// For "standard" strings (in particular, members of the 'Strings' namespace in
+// liboslexec/oslexec_pvt.h), we declare fixed tags to make it easier to write
+// CUDA code that uses them.
+//
+// Strings that do not need to be shared between liboslexec and the renderer,
+// but which have special significance in the renderer (such as custom closure
+// parameters), can be registered with the shading system at runtime via
+// register_string_tag().
+//
+// For other strings, the tag is the ustring hash.
+//
+// A pre-declared device_string is listed:
+//
+// 1) In the StringTags enum in this file. This file can be included in your
+//    OptiX/CUDA renderer to make the tags available.
+//
+// 2) In the definitions in liboslexec/device_string.cpp. These are linked with
+//    the other 'shadeops' sources (opnoise.cpp, etc) and made available as
+//    global symbols to executing shaders.
+//
+// 3) In ShadingSystemImpl::setup_string_tags(), which registers the enum value
+//    as the tag for that ustring. This lets libsolexec create a device_string
+//    with the appropriate tag during shader compilation.
+
+
 OSL_NAMESPACE_ENTER
 
 
-// For the universe of "standard" strings, we want to fix the tags so that OSL
-// library code (shadeops, noise gen, etc.), compiled shaders, and the renderer
-// can agree on the tag associated with each "canonical" string.
-enum StringTags: uint64_t {
-    EMPTY_STRING = 0,
-    TEST_STRING,
-    UNKNOWN_STRING = ~0u
-};
-
-
 struct device_string {
-    OSL_HOSTDEVICE uint64_t tag ()
+    OSL_HOSTDEVICE uint64_t tag () const
     {
         return m_tag;
     }
 
     OSL_HOSTDEVICE const char* c_str () const
     {
-        return m_ptr;
+        return m_chars;
     }
 
     OSL_HOSTDEVICE bool operator== (const device_string& other) const
@@ -65,7 +95,14 @@ struct device_string {
     }
 
     uint64_t    m_tag;
-    const char* m_ptr;
+    const char* m_chars;
+};
+
+
+enum StringTags: uint64_t {
+    EMPTY_STRING = 0,
+    TEST_STRING,
+    UNKNOWN_STRING = ~0u
 };
 
 

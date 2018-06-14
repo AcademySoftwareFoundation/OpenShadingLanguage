182c02b28a6ffee8744b48e39d378dca505f9287
Larry Gritz <lg@larrygritz.com>
2018-06-14 12:11:19 -0700

    Fix test output to not fail on Windows

--- testsuite/error-dupes/run.py
+++ testsuite/error-dupes/run.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
-command = "echo 'Without repeated errors:' >> out.txt 2>&1 ;\n"
+command = "echo Without repeated errors:>> out.txt 2>&1 ;\n"
 command += testshade("-g 2 2 test")
 
-command += "echo 'With repeated errors:' >> out.txt 2>&1 ;\n"
+command += "echo With repeated errors:>> out.txt 2>&1 ;\n"
 command += testshade("--options error_repeats=1 -g 2 2 test")

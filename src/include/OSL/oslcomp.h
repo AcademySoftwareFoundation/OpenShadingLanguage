2332f60ec5c98beeb131229c88eaa4a12ff49c01
Larry Gritz <lg@larrygritz.com>
2018-11-26 14:26:06 -0800

    Improve error reporting when compiling from memory buffer (#937)
    The `OSLCompiler::compile_buffer()` method, which is used to compile
    osl source code from a memory buffer, had unhelpful error messages
    such as:
    
        <buffer>:11: error: Unknown struct name: x
    
    because of course, there was no filename, just a memory buffer. This
    is ugly and confusing to users.
    
    This patch adds an optional parameter to compile_buffer() that lets you
    supply a "filename" to use for error reporting of the mem-buffer-compiled
    shader. This makes pretty error messages if you supply a good name:
    
        zap.osl:11: error: Unknown struct name: x
    
    An application of this is shown with the changes in osltoy.
    
    For my own debugging purposes, I also implemented a `-buffer` option to
    oslc, which just forces a file read, compile-from-buffer, and then file
    write, so that it's easier for me to exercise the buffer code path.

--- src/include/OSL/oslcomp.h
+++ src/include/OSL/oslcomp.h
@@ -47,18 +47,24 @@ public:
     OSLCompiler (ErrorHandler *errhandler=NULL);
     ~OSLCompiler ();
 
-    /// Compile the given file, using the list of command-line options.
+    /// Compile the given file, using the list of command-line options. The
+    /// stdoslpath parameter provides a custom path for finding stdosl.h.
     /// Return true if ok, false if the compile failed.
     bool compile (string_view filename,
                   const std::vector<std::string> &options,
                   string_view stdoslpath = string_view());
 
     /// Compile the given source code buffer, using the list of command-line
-    /// options, placing the resulting "oso" in osobuffer. Return true if
-    /// ok, false if the compile failed.
+    /// options, placing the resulting "oso" in osobuffer. The stdoslpath
+    /// parameter provides a custom path for finding stdosl.h. The filename
+    /// optionally provides a name for the buffer, used for error reporting
+    /// (the compile() from file method would have used the name of the
+    /// actual file for this purpose). Return true if ok, false if the
+    /// compile failed.
     bool compile_buffer (string_view sourcecode, std::string &osobuffer,
                          const std::vector<std::string> &options,
-                         string_view stdoslpath = string_view());
+                         string_view stdoslpath = string_view(),
+                         string_view filename = string_view());
 
     /// Return the name of our compiled output (must be called after
     /// compile()).

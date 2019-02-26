bcdc9ccdbbbb76d7aacafc47cf31a719ec964a73
Tim Grant <tgrant@nvidia.com>
2019-02-25 17:19:49 -0700

    Use ustring::format instead of sprintf when constructing CUDA variable names.

--- src/liboslexec/backendllvm.cpp
+++ src/liboslexec/backendllvm.cpp
@@ -499,12 +499,12 @@ BackendLLVM::getOrAllocateCUDAVariable (const Symbol& sym)
         }
     }
     else {
-        ustring var_name = ustring::sprintf ("%s_%s_%d_%s_%d",
-                                             sym.name(),
-                                             group().name(),
-                                             group().id(),
-                                             inst()->layername(),
-                                             sym.layer());
+        ustring var_name = ustring::format ("%s_%s_%d_%s_%d",
+                                            sym.name(),
+                                            group().name(),
+                                            group().id(),
+                                            inst()->layername(),
+                                            sym.layer());
 
         // Leading dollar signs are not allowed in PTX variable names,
         // so prepend an underscore.

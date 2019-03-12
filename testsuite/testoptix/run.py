23e40ebfa67e25a9515a7a70d078a2a103560fe0
Larry Gritz <lg@larrygritz.com>
2019-03-12 13:46:28 -0700

    testoptix enhancements to make more analogous to testrender
    * Change -r to -res to match testrender (and for that matter, make -res
      work for testshade, not -g). (In both cases, the old oddballs are
      silent synonyms.)
    
    * Respond to several additional OSL options, and set them the same
      was as testrender
    
    * Allow --iters (much like it works in testshade) to make it easy to
      run all the rays many times to make accurate timing easier.
    
    Also do a little bit of synchronizing of near-identical code between
    testshade, testrender, and testoptix. And synchronize some of the
    analogous command line arguments (e.g., -res for everybody, not -res
    for testrender and -r for testoptix and -g for testsahde).

--- testsuite/testoptix/run.py
+++ testsuite/testoptix/run.py
@@ -3,10 +3,10 @@
 failthresh = 0.03   # allow a little more LSB noise between platforms
 failpercent = .5
 outputs  = [ "out.exr", "out.txt" ]
-command  = testoptix("-r 320 240 scene.xml out.exr")
-command += testoptix("-r 1 1 test_print.xml dummy.exr")
-command += testoptix("-r 1 1 test_compare.xml dummy.exr")
-command += testoptix("-r 1 1 test_assign.xml dummy.exr")
-command += testoptix("-r 1 1 test_assign_02.xml dummy.exr")
-command += testoptix("-r 1 1 test_str_ops.xml dummy.exr")
-command += testoptix("-r 1 1 test_userdata_string.xml dummy.exr")
+command  = testoptix("-res 320 240 scene.xml out.exr")
+command += testoptix("-res 1 1 test_print.xml dummy.exr")
+command += testoptix("-res 1 1 test_compare.xml dummy.exr")
+command += testoptix("-res 1 1 test_assign.xml dummy.exr")
+command += testoptix("-res 1 1 test_assign_02.xml dummy.exr")
+command += testoptix("-res 1 1 test_str_ops.xml dummy.exr")
+command += testoptix("-res 1 1 test_userdata_string.xml dummy.exr")

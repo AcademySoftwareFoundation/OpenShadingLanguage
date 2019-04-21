8ae87917befab2303c7f878512098d4aa9d66537
Frederich Munch <marsupial@users.noreply.github.com>
2019-04-21 10:55:35 -0700

    Allow testsuite to run (and hopefully pass) when built outside the source tree.

--- testsuite/texture-wrap/run.py
+++ testsuite/texture-wrap/run.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-command += "maketx ../common/textures/mandrill.tif --wrap clamp -o mandrill.tx ; \n"
+command += maketx("../common/textures/mandrill.tif --wrap clamp -o mandrill.tx")
 command += testshade("-g 64 64 --center -od uint8 -o Cblack black.tif -o Cclamp clamp.tif -o Cperiodic periodic.tif -o Cmirror mirror.tif -o Cdefault default.tif test")
 
 outputs = [ "out.txt", "black.tif", "clamp.tif", "periodic.tif", "mirror.tif",

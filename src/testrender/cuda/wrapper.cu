891fd6c47f31b7b5886678a4c6047b186ec19ac3
Tim Grant <tgrant@nvidia.com>
2019-09-18 15:27:43 -0600

    Add an OptiX test for string closure params using the microfacet distribution.

--- src/testrender/cuda/wrapper.cu
+++ src/testrender/cuda/wrapper.cu
@@ -151,20 +151,18 @@ float3 process_closure(const OSL::ClosureColor* closure_tree)
         }
 
         case MICROFACET_ID: {
-#if 0
             const char* mem = (const char*)((OSL::ClosureComponent*) cur)->data();
             const char* dist_str = *(const char**) &mem[0];
-            if (launch_index.x == launch_dim.x / 2 && launch_index.y == launch_dim.y / 2) {
+#if 0
+            if (launch_index.x == launch_dim.x / 2 && launch_index.y == launch_dim.y / 2)
                 printf ("microfacet, dist: %s\n", HDSTR(dist_str).c_str());
-                // Comparisons between the closure variable and "standard"
-                // strings are possible
-                if (HDSTR(dist_str) == OSL::DeviceStrings::default_)
-                    printf("dist is default\n");
-            }
 #endif
 
-            result += ((OSL::ClosureComponent*) cur)->w * weight;
-            cur = NULL;
+            if (HDSTR(dist_str) == OSL::DeviceStrings::default_)
+                return make_float3(0.0f, 1.0f, 1.0f);
+            else
+                return make_float3(1.0f, 0.0f, 1.0f);
+
             break;
         }
 

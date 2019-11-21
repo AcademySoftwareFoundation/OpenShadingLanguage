b0e0b7fb955bcffb6ad34bd42c1a5cafedb818a4
Larry Gritz <lg@larrygritz.com>
2019-11-20 17:48:16 -0800

    Fix locale ctr for old compilers

--- src/testshade/testshade.cpp
+++ src/testshade/testshade.cpp
@@ -1003,7 +1003,7 @@ test_shade (int argc, const char *argv[])
 
     // For testing purposes, allow user to set global locale
     if (localename.size()) {
-        std::locale::global (std::locale(localename));
+        std::locale::global (std::locale(localename.c_str()));
         if (debug || verbose)
             printf("testshade: locale '%s', floats look like: %g\n",
                    localename.c_str(), 3.5);

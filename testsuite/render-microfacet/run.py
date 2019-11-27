53e45f48f175518ee1a8946921c31c629df9f866
Larry Gritz <lg@larrygritz.com>
2019-11-27 14:47:28 -0800

    Adjust failure threshold for changed noise

--- testsuite/render-microfacet/run.py
+++ testsuite/render-microfacet/run.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
-failthresh = 0.005
-failpercent = 0.1
+failthresh = 0.01
+failpercent = 0.01
+hardfail = 0.026
 outputs = [ "out.exr" ]
 command = testrender("-r 320 240 -aa 8 scene.xml out.exr")

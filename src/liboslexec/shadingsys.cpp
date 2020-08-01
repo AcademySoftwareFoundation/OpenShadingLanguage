2db64f902d1e61437dea2e7dd9abeecf15fedcb8
etheory <evolutionarytheory@gmail.com>
2020-08-02 01:16:07 +1000

    Remove null check from symbol_address

--- src/liboslexec/shadingsys.cpp
+++ src/liboslexec/shadingsys.cpp
@@ -387,7 +387,7 @@ const void*
 ShadingSystem::symbol_address (const ShadingContext &ctx,
                                const ShaderSymbol *sym) const
 {
-    return sym ? ctx.symbol_data (*(const Symbol *)sym) : NULL;
+    return ctx.symbol_data (*(const Symbol *)sym);
 }
 
 

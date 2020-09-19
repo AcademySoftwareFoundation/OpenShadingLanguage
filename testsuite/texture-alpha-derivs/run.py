78257585575d2c0c8ea0abd9ea4425dc9885f004
Larry Gritz <lg@larrygritz.com>
2020-09-19 10:31:52 -0700

    Fix derivatives of texture calls when only Dalpha is needed
    Luke Emrose noticed that for texture calls that retrieved alpha, if
    derivatives were later taken *only* of the alpha but not the main
    color result of the texture call, those derivs would always end up 0,
    the texture call not noticing that it needed to call the version of
    texture that returns derivatives.
    
    This patch provides a fix. It was a simple matter of it testing only
    whether main result derivs were needed, when determining which variety
    of texture must be called.
    
    Curiously, this was already done correctly for texture3d!
    
    Added testsuite/texture-alpha-derivs to test this specific situation.
    
    Signed-off-by: Larry Gritz <lg@larrygritz.com>

---
+++ testsuite/texture-alpha-derivs/run.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+# Copyright Contributors to the Open Shading Language project.
+# SPDX-License-Identifier: BSD-3-Clause
+# https://github.com/imageworks/OpenShadingLanguage
+
+# Set up a texture that is a gradient image with a distinct alpha gradient
+# of 0.5 from left to right and 0.25 right to left, and no change in value
+# for R,G,B.
+command += oiiotool("-pattern fill:topleft=0.125,0.25,0.5,0:topright=0.125,0.25,0.5,0.5:bottomleft=0.125,0.25,0.5,0.25:bottomright=0.125,0.25,0.5,0.75 64x64 4 -d half -o alpharamp.exr")
+
+command += testshade("-g 64 64 --center -od uint8 -o Cout out.tif test")
+outputs = [ "out.txt", "out.tif" ]

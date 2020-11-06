089328218f0417c880b67b90b697fe3f72265511
Reza Aarabi <madoodia@gmail.com>
2020-11-06 12:27:14 -0800

    remove pythonpath

--- build_scripts/osl_release_sln.bat
+++ build_scripts/osl_release_sln.bat
@@ -6,6 +6,6 @@ call osl_env_vars_setup.bat
 cls
 
 set PATH=%PYTHON_LOCATION%;%PYTHON_LOCATION%/Scripts;%QT_LOCATION%/bin;%QT_LOCATION%/lib;%BASE_LOCATION%/osl_release/bin;%BASE_LOCATION%/osl_release/lib;%PATH%
-set PYTHONPATH=%PYTHON_LOCATION%/Lib/site-packages;%BASE_LOCATION%/osl_release/lib/python3.7;
+set PYTHONPATH=%PYTHON_LOCATION%/Lib/site-packages;
 
 %BASE_LOCATION%\osl_release\build\OpenShadingLanguage\OSL.sln

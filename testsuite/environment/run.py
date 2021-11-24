4a51c8f50131b108520352789ba68ae9a2046b10
Alex M. Wells <alex.m.wells@intel.com>
2021-11-24 12:36:47 -0800

    Removed redundant environment test as texture-environment covers it. Enabled BATCHED execution of texture-environment
    Signed-off-by: Alex M. Wells <alex.m.wells@intel.com>

--- testsuite/environment/run.py
+++
@@ -1,9 +0,0 @@
-# Copyright Contributors to the Open Shading Language project.
-# SPDX-License-Identifier: BSD-3-Clause
-# https://github.com/AcademySoftwareFoundation/OpenShadingLanguage
-#!/usr/bin/env python 
-
-command += oiiotool ("-pattern fill:topleft=0,0,0:topright=1,0,0:bottomleft=0,1,0:bottomright=1,1,1 256x128 3 -d uint8 -oenv ramp.env")
-
-command += testshade("-g 256 256 -od uint8 -o Cout out.tif test")
-outputs = [ "out.tif" ]

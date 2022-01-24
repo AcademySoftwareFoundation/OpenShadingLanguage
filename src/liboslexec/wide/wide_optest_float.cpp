e07b7ecaca2ffef33ab4b2ba26f0ff97aa4f7246
Alex M. Wells <alex.m.wells@intel.com>
2022-01-24 12:02:12 -0800

    Add new files missed in original commit
    Signed-off-by: Alex M. Wells <alex.m.wells@intel.com>

---
+++ src/liboslexec/wide/wide_optest_float.cpp
@@ -0,0 +1,45 @@
+// Copyright Contributors to the Open Shading Language project.
+// SPDX-License-Identifier: BSD-3-Clause
+// https://github.com/AcademySoftwareFoundation/OpenShadingLanguage
+
+/////////////////////////////////////////////////////////////////////////
+/// \file
+///
+/// Shader interpreter implementation of Floating Point Test operations
+/// NOTE: many functions are left as LLVM IR, but some are better to 
+/// execute from the library to take advantage of compiler's small vector
+/// math library versions.
+///
+/////////////////////////////////////////////////////////////////////////
+
+#include <OSL/oslconfig.h>
+
+#include <OSL/wide.h>
+#include <OSL/sfmath.h>
+#include <OpenImageIO/fmath.h>
+
+OSL_NAMESPACE_ENTER
+namespace __OSL_WIDE_PVT {
+
+OSL_USING_DATA_WIDTH(__OSL_WIDTH)
+
+#include "define_opname_macros.h"
+
+#define __OSL_XMACRO_ARGS (isnan,OIIO::isnan)
+//#define __OSL_XMACRO_ARGS (isnan,std::isnan)
+#include "wide_optest_float_xmacro.h"
+
+#define __OSL_XMACRO_ARGS (isinf, OIIO::isinf)
+//#define __OSL_XMACRO_ARGS (isinf, sfm::isinf)
+//#define __OSL_XMACRO_ARGS (isinf, std::isinf)
+#include "wide_optest_float_xmacro.h"
+
+#define __OSL_XMACRO_ARGS (isfinite,OIIO::isfinite)
+//#define __OSL_XMACRO_ARGS (isfinite,std::isfinite)
+#include "wide_optest_float_xmacro.h"
+
+
+} // namespace __OSL_WIDE_PVT
+OSL_NAMESPACE_EXIT
+
+#include "undef_opname_macros.h"

7366046f47fbeee1b759ded7eb6e6e7a2fafb604
Larry Gritz <lg@larrygritz.com>
2022-03-04 10:17:46 -0800

    CI: Add test that uses the Intel icc/icpc compiler. (#1459)
    (and a bunch of minor changes to support this)
    
    Signed-off-by: Larry Gritz <lg@larrygritz.com>

--- testsuite/render-uv/run.py
+++ testsuite/render-uv/run.py
@@ -7,4 +7,4 @@
 failthresh = 0.03   # allow a little more LSB noise between platforms
 failpercent = .5
 outputs = [ "out.exr" ]
-command = testrender("-r 320 240 -aa 4 scene.xml out.exr")
+command = testrender("-r 320 240 -aa 8 scene.xml out.exr")

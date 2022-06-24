faefe603f11b2c97da5f1f423d2d58994c2a9b85
Adrien Herubel <adrien.herubel@autodesk.com>
2022-06-24 11:07:20 +0100

    Fix permission error and build errors when running serialize-bc.py on Linux
    Signed-off-by: Adrien Herubel <adrien.herubel@autodesk.com>

--- src/build-scripts/serialize-bc.py
+++ src/build-scripts/serialize-bc.py
@@ -1,3 +1,4 @@
+#!/bin/env python
 # Copyright Contributors to the Open Shading Language project.
 # SPDX-License-Identifier: BSD-3-Clause
 # https://github.com/AcademySoftwareFoundation/OpenShadingLanguage

d0d547f979019ea424be29cbea3e4adc345c56a2
Steena Monteiro <steena.monteiro@intel.com>
2022-09-22 12:45:41 -0700

    Added missing Copyright Contributors license to shading_state_uniform.h
    Signed-off-by: Steena Monteiro <steena.monteiro@intel.com>

--- src/liboslexec/shading_state_uniform.h
+++ src/liboslexec/shading_state_uniform.h
@@ -1,3 +1,7 @@
+// Copyright Contributors to the Open Shading Language project.
+// SPDX-License-Identifier: BSD-3-Clause
+// https://github.com/AcademySoftwareFoundation/OpenShadingLanguage
+
 #include "opcolor.h"
 
 OSL_NAMESPACE_ENTER

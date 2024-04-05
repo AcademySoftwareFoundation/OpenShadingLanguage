b6c8dde7b9d0d848e6882eb312d926192c9d343d
sfriedmapixar <sfriedma@pixar.com>
2024-04-05 08:28:53 -0700

    Make isconnected() work with downstream renderer "connections." (#1782)
    The isconnected() returns 2 when used on an output that is connected to another downstream shader node -- this can be used to limit computation to only the outputs used.  However, that misses the important case of where the renderer has declared a shader node output as a renderer output, which happens often when using OSL purely for pattern generation purposes.  This change makes that case also return 2.
    
    The isconnected test has been updated to include testing this functionality.
    
    Signed-off-by: Stephen Friedman <sfriedma@pixar.com>

--- testsuite/isconnected/run.py
+++ testsuite/isconnected/run.py
@@ -4,9 +4,11 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # https://github.com/AcademySoftwareFoundation/OpenShadingLanguage
 
-command  = testshade("--layer upstream upstream --layer downstream test " +
+command  = testshade("-g 1 1 -od uint8 -o Fout out.tif " +
+                     "--layer upstream upstream --layer downstream test " +
                      "--connect upstream out downstream a " +
-                     "--connect upstream struct1 downstream mystruct1")
+                     "--connect upstream struct1 downstream mystruct1" 
+                     )
 
 # Run again, this time considering userdata (lockgeom=0) parameters to be
 # isconnected(). This time, parameter c should look like a connection.

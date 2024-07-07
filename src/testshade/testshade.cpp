c8818baa082f71f18703c189802b75bb10de0f8e
Larry Gritz <lg@larrygritz.com>
2024-07-07 08:17:36 -0700

    cleanup: Swich away from deprecated OIIO function names
    Signed-off-by: Larry Gritz <lg@larrygritz.com>

--- src/testshade/testshade.cpp
+++ src/testshade/testshade.cpp
@@ -1418,9 +1418,9 @@ batched_save_outputs(SimpleRenderer* rend, ShadingSystem* shadingsys,
                     int y    = by[batchIndex];
                     int data = batchResults[batchIndex];
                     float pixel[1];
-                    OIIO::convert_types(TypeDesc::BASETYPE(t.basetype), &data,
-                                        TypeDesc::FLOAT, &pixel[0],
-                                        1 /*nchans*/);
+                    OIIO::convert_pixel_values(TypeDesc::BASETYPE(t.basetype),
+                                               &data, TypeDesc::FLOAT,
+                                               &pixel[0], 1 /*nchans*/);
                     outputimg->setpixel(x, y, &pixel[0]);
                     if (print_outputs) {
                         *oStreams[batchIndex]
@@ -1442,9 +1442,9 @@ batched_save_outputs(SimpleRenderer* rend, ShadingSystem* shadingsys,
                         intPixel[c] = batchResults[batchIndex][c];
                     }
 
-                    OIIO::convert_types(TypeDesc::BASETYPE(t.basetype),
-                                        intPixel, TypeDesc::FLOAT, floatPixel,
-                                        3 /*nchans*/);
+                    OIIO::convert_pixel_values(TypeDesc::BASETYPE(t.basetype),
+                                               intPixel, TypeDesc::FLOAT,
+                                               floatPixel, 3 /*nchans*/);
                     outputimg->setpixel(x, y, floatPixel);
                     if (print_outputs) {
                         (*oStreams[batchIndex])

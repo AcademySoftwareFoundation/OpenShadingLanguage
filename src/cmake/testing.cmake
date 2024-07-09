c85a812cdb662e8e1be807960cf7f39adb622bf9
Chris Kulla <ckulla@gmail.com>
2024-07-09 11:05:40 -0700

    Run render tests using the optimized variant only, and skip the bitcode version which is not implemented in testrender
    Signed-off-by: Chris Kulla <ckulla@gmail.com>

--- src/cmake/testing.cmake
+++ src/cmake/testing.cmake
@@ -127,6 +127,7 @@ macro ( TESTSUITE )
         # Run the test unoptimized, unless it matches a few patterns that
         # we don't test unoptimized (or has an OPTIMIZEONLY marker file).
         if (NOT _testname MATCHES "optix"
+            AND NOT _testname MATCHES "render"
             AND NOT EXISTS "${_testsrcdir}/NOSCALAR"
             AND NOT EXISTS "${_testsrcdir}/BATCHED_REGRESSION"
             AND NOT EXISTS "${_testsrcdir}/OPTIMIZEONLY")
@@ -137,6 +138,7 @@ macro ( TESTSUITE )
         # services keeping unoptimized, unless it matches a few patterns that
         # we don't test unoptimized (or has an OPTIMIZEONLY marker file).
         if (NOT _testname MATCHES "optix"
+            AND NOT _testname MATCHES "render"
             AND NOT EXISTS "${_testsrcdir}/NOSCALAR"
             AND NOT EXISTS "${_testsrcdir}/BATCHED_REGRESSION"
             AND NOT EXISTS "${_testsrcdir}/OPTIMIZEONLY"
@@ -159,6 +161,7 @@ macro ( TESTSUITE )
         # optimization, triggered by setting TESTSHADE_OPT env variable.
         # Skip OptiX-only tests and those with a NOOPTIMIZE marker file.
         if (NOT _testname MATCHES "optix"
+            AND NOT _testname MATCHES "render"
             AND NOT EXISTS "${_testsrcdir}/NOSCALAR"
             AND NOT EXISTS "${_testsrcdir}/BATCHED_REGRESSION"
             AND NOT EXISTS "${_testsrcdir}/NOOPTIMIZE"

3fc4a5450121f7e1a5fa217c9fdcd51cdf32652e
Larry Gritz <lg@larrygritz.com>
2025-02-23 12:11:59 -0800

    ci: improve clang-format
    Make a new run-clang-format.bash utility to issue the commands
    for the right files. No "build" is requred, and the only dependencies
    we need are the minimum it takes to run clang-format itself.
    
    These changes reduce the approximate total elapsed time of a CI
    clang-format job from 8:00+ to under 0:30.
    
    Signed-off-by: Larry Gritz <lg@larrygritz.com>

--- testsuite/example-cuda/rend_lib.cu
+++ testsuite/example-cuda/rend_lib.cu
@@ -230,8 +230,8 @@ osl_bind_interpolated_param(void* sg_, OSL::ustring_pod name, long long type,
     char status = *userdata_initialized;
     if (status == 0) {
         bool ok               = rend_get_userdata(HDSTR(name), userdata_data,
-                                    symbol_data_size, (*(OSL::TypeDesc*)&type),
-                                    userdata_index);
+                                                  symbol_data_size, (*(OSL::TypeDesc*)&type),
+                                                  userdata_index);
         *userdata_initialized = status = 1 + ok;
     }
     if (status == 2) {

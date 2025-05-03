506d370517faf977ef05a32d99d684cf9b675310
Larry Gritz <lg@larrygritz.com>
2025-05-02 22:23:50 -0700

    build: Remove unused symbol that was removed from OIIO main (#1984)
    This symbol was for an architecture that never shipped mainstream,
    we don't need to look for it.
    
    Signed-off-by: Larry Gritz <lg@larrygritz.com>

--- src/liboslexec/shadingsys.cpp
+++ src/liboslexec/shadingsys.cpp
@@ -1568,7 +1568,6 @@ osl_simd_caps()
     if (OIIO_AVX512DQ_ENABLED)   caps.emplace_back ("avx512dq");
     if (OIIO_AVX512IFMA_ENABLED) caps.emplace_back ("avx512ifma");
     if (OIIO_AVX512PF_ENABLED)   caps.emplace_back ("avx512pf");
-    if (OIIO_AVX512ER_ENABLED)   caps.emplace_back ("avx512er");
     if (OIIO_AVX512CD_ENABLED)   caps.emplace_back ("avx512cd");
     if (OIIO_AVX512BW_ENABLED)   caps.emplace_back ("avx512bw");
     if (OIIO_AVX512VL_ENABLED)   caps.emplace_back ("avx512vl");

1fda124b3a12f1ce142d9f39754f4d39c9000f26
Alejandro Conty <aconty@imageworks.com>
2025-05-25 13:06:32 +0200

    Update license in src/libbsdl/src/genluts.cpp
    Co-authored-by: Larry Gritz <lg@larrygritz.com>
    Signed-off-by: Alejandro Conty <aconty@imageworks.com>

--- src/libbsdl/src/genluts.cpp
+++ src/libbsdl/src/genluts.cpp
@@ -1,3 +1,8 @@
+// Copyright Contributors to the Open Shading Language project.
+// SPDX-License-Identifier: BSD-3-Clause
+// https://github.com/AcademySoftwareFoundation/OpenShadingLanguage
+
+
 #define BAKE_BSDL_TABLES 1
 
 #define BSDL_UNROLL()  // Do nothing

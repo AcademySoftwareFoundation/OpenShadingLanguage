ccfa903c159f35ea4d1089782b1d1361535dae73
Alejandro Conty <aconty@imageworks.com>
2025-05-25 13:07:49 +0200

    Update license in src/libbsdl/src/jakobhanika_luts.cpp
    Co-authored-by: Larry Gritz <lg@larrygritz.com>
    Signed-off-by: Alejandro Conty <aconty@imageworks.com>

--- src/libbsdl/src/jakobhanika_luts.cpp
+++ src/libbsdl/src/jakobhanika_luts.cpp
@@ -5,7 +5,7 @@
 //
 // pbrt is Copyright(c) 1998-2020 Matt Pharr, Wenzel Jakob, and Greg Humphreys.
 // The pbrt source code is licensed under the Apache License, Version 2.0.
-// SPDX: Apache-2.0
+// SPDX-License-Identifier: Apache-2.0
 
 #include <assert.h>
 

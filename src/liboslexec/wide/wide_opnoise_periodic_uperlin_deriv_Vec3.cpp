0a0e79d58f859259583ec17123158647ea32f28d
Larry Gritz <lg@larrygritz.com>
2026-02-12 22:15:33 -0800

    build: Remove support for deprecated Intel icc compiler
    Intel icc is deprecated and hasn't had a release for a few years. It's
    holding us back, both by making us work around an ever growing number
    of icc bugs and limitation that will never be fixed, as well as not
    allowing us to upgrade minimum versions of certain dependencies,
    because icc can't correctly compile newer versions (as an example, it
    cannot use a 'fmt' library newer than the oldest we support, 7.0).
    
    So it's time to thank icc for its service and put it on the ice floe
    for the polar bears to eat. This is of course in main (future OSL
    1.16), and will not be backported to release branches, since we never
    stop support of a dependency or toolchain of existing releases. People
    requiring icc for whatever reason may keep using OSL 1.15 or older.
    
    We will continue to support and test icx, the fully supported Intel
    LLVM-based compiler.
    
    This PR also removes some test reference output that was only needed
    for icc.
    
    Signed-off-by: Larry Gritz <lg@larrygritz.com>

--- src/liboslexec/wide/wide_opnoise_periodic_uperlin_deriv_Vec3.cpp
+++ src/liboslexec/wide/wide_opnoise_periodic_uperlin_deriv_Vec3.cpp
@@ -24,17 +24,7 @@ template<> struct BatchedCGPolicy<Param::WDV, Param::WDV, Param::WV> {
 };
 template<>
 struct BatchedCGPolicy<Param::WDV, Param::WDV, Param::WDF, Param::WV, Param::WF> {
-#if ((defined(_WIN32) || defined(_WIN64)) && defined(__INTEL_COMPILER))
-    // Avoid stack overflow on windows build because compiler has hard coded stack limit.
-    // By not forcing everything to recursively inline and not explicitly vectorizing,
-    // the compiler uses less stack space at the cost of NOT creating a properly SIMD optimized function.
-    // Linux & OSX can increase stacksize before building
-
-    static constexpr int simd_threshold
-        = __OSL_WIDTH + 1;  // Make SIMD code path unreachable
-#else
     static constexpr int simd_threshold = 6;
-#endif
 };
 }  // namespace
 

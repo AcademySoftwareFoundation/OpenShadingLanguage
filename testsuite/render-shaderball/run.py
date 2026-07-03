7aabd1702e33f8c5349704ea67d6dbe1ed020bf7
Jonathan Peters <jmp22@sfu.ca>
2026-07-03 04:09:52 +0000

    test(testrender): Add render-shaderball testsuite entry
    Add a new testrender-based test scene featuring the ASWF USD Working
    Group StandardShaderBall asset and scene, rendered using testrender.
    
    Scene geometry was converted from USDA format to OBJ format in Blender.
    The scene uses testrender's support for ShaderGroup/MTL material group
    name matching to assign shaders to the scene. Lighting is created from scene
    geometry, with an emission shader applied to planes to mimic the area lights
    of the USD scene. Camera angle, textures, and lighting have been tweaked to
    provide a close, but not exact, replica of the USD scene.
    
    The new render-shaderball test has been integrated into the testsuite,
    referencing an image generated at 320x240 px, using setting -aa 4 on the CPU.
    
    Signed-off-by: Jonathan Peters <jmp22@sfu.ca>

---
+++ testsuite/render-shaderball/run.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+# Copyright Contributors to the Open Shading Language project.
+# SPDX-License-Identifier: BSD-3-Clause
+# https://github.com/AcademySoftwareFoundation/OpenShadingLanguage
+
+failthresh = 0.01
+failpercent = 1
+allowfailures = 3
+idiff_program = "idiff"
+
+outputs = [ "out.exr" ]
+command = testrender("-r 320 240 -aa 4 scene.xml out.exr")
